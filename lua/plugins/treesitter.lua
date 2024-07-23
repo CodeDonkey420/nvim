a0eb10f1c14457dcaf74ae2095ff507b54b04692
Maxim Walther <mail@maxim-walther.de>
2024-07-23 18:49:06 +0200

    upload to github

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+local config = function()
+	require("nvim-treesitter.configs").setup({
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"markdown",
+			"json",
+			"html",
+			"css",
+			"markdown",
+			"bash",
+			"lua",
+			"gitignore",
+			"python",
+      "rust",
+      "c",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+	})
+end
+
+return {
+	"nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  config = config
+}
+
